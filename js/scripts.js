e03844738d18e2427a31b5112694dfed898d030c
kirandash <thekirankumardash@gmail.com>
2017-02-12 13:10:03 +0530

    2 Basic Map Options

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,57 @@ function loadMap() {
     var mapOptions = {
 
         //Zoom on load
-        zoom: 5,
+        zoom: 18,
 
         //Map center
-        center: new google.maps.LatLng(39.828127,-98.579404)
+        center: new google.maps.LatLng(25.767,-80.1363),
 
+        //Limit Min/Max Zoom
+        /*minZoom: 10,
+        maxZoom: 12,*/
+
+        //Map control
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            /* Map controls can be set to DROPDOWN MENU from default horizontal */
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            /* Map control options */
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP,
+                         google.maps.MapTypeId.SATELLITE,
+                         google.maps.MapTypeId.HYBRID,
+                         google.maps.MapTypeId.TERRAIN
+                        ],
+            /* Map control positions - 12 choices */
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
+
+        //Set MapType (Default Map Layer before choosing any of the 4 options)
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+
+        //Only valid for SATELLITE and TERRAIN, Note that min and max zoom should be removed for this
+        tilt: 45,
+
+        //Zoom Controls
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+
+        //Pan Controls
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.TOP_LEFT
+        },
+
+        //Street View Control
+        streetViewControl: true,
+
+        //Overview map
+        overviewMapControl: true,
+        overviewMapControlOptions: {
+            opened: true
+        }
     };
 
     //Get the id of the map container div
@@ -25,3 +71,7 @@ function loadMap() {
 
 //Load the map
 google.maps.event.addDomListener(window, 'load', loadMap());
+       
+
+
+
