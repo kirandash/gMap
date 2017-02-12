6ff0d3f29cae38112760bd04328a6e0b85d5ef92
kirandash <thekirankumardash@gmail.com>
2017-02-12 13:35:28 +0530

    3 Map Styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,49 @@
 //Global map variable
 var map;
 
+//Style Elements (Use snazzymaps.com)
+var mapStyle = [
+    {
+        //If featuretype is not defined this style will apply to all features on map
+        'stylers':[
+            {'saturation':-100}, //decrease color saturation
+            {'gamma':1}
+        ]
+    },
+    {
+        /* This style for all features but specific elements - labels text */
+        'elementType':'labels.text.stroke',
+        'stylers':[
+            {'visibility':'on'}
+        ]
+    },
+    {
+        'featureType':'road',
+        'elementType':'geometry',
+        'stylers':[
+            {'visibility':'simplified'}
+            /* If turned on all the secondary roads will be displayed */
+        ]
+    },
+    {
+        'featureType':'water',
+        'stylers':[
+            /* Visibility should be on to overwrite the global stylers */
+            {'visibility':'on'},
+            {'saturation':50},
+            {'gamma':0},
+            {'hue':'#50a5d1'}
+        ]
+    },
+    {
+        'featureType':'landscape',
+        'elementType':'all',
+        'stylers':[
+            {'color':'#e2e2e2'}
+        ]
+    }
+];
+
 //Function run on DOM load
 function loadMap() {
     
@@ -33,7 +76,7 @@ function loadMap() {
         },
 
         //Set MapType (Default Map Layer before choosing any of the 4 options)
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
 
         //Only valid for SATELLITE and TERRAIN, Note that min and max zoom should be removed for this
         tilt: 45,
@@ -58,7 +101,10 @@ function loadMap() {
         overviewMapControl: true,
         overviewMapControlOptions: {
             opened: true
-        }
+        },
+
+        //Set the map style
+        styles: mapStyle
     };
 
     //Get the id of the map container div
